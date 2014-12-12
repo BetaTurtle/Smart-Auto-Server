09b6e50833843da0e26e19defe14293ba4caa927
jun <junaidvayoli@gmail.com>
2014-12-12 20:44:33 +0530

    user db and seed

--- app/database/migrations/2014_12_11_195159_create_users_table.php
+++ app/database/migrations/2014_12_11_195159_create_users_table.php
@@ -15,6 +15,10 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->string('name', 20);
+		    $table->string('username', 20);
+		    $table->string('email', 100)->unique();
+		    $table->string('password', 64);
 			$table->timestamps();
 		});
 	}
---
+++ app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+class UserTableSeeder extends Seeder
+{
+
+	public function run()
+	{
+		DB::table('users')->delete();
+		User::create(array(
+			'name'     => 'Junaid Babu',
+			'username' => 'jun',
+			'email'    => 'me@junaidbabu.in',
+			'password' => Hash::make('awesome'),
+		));
+	}
+
+}
